a314ccdf63e404b8f7216c8c954af28be10bf79e
Ivan Cline <117295656+ivan-cline1@users.noreply.github.com>
2024-01-07 18:02:42 -0700

    Add files via upload

---
+++ src/js/offscreen.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video");
+  var runningAverages = {
+    angry: 0,
+    fearful: 0,
+    disgusted: 0,
+    surprised: 0,
+    neutral: 0,
+    happy: 0,
+    sad: 0,
+  };
+
+  chrome.runtime.onMessage.addListener((msg) => {
+    if (!msg.offscreen) {
+      return;
+    }
+    switch (msg.type) {
+      case "empathize":
+        Promise.all([
+          faceapi.nets.tinyFaceDetector.loadFromUri("/emotion_recognition/models"),
+          faceapi.nets.faceLandmark68Net.loadFromUri("/emotion_recognition/models"),
+          faceapi.nets.faceRecognitionNet.loadFromUri("/emotion_recognition/models"),
+          faceapi.nets.faceExpressionNet.loadFromUri("/emotion_recognition/models"),
+        ]).then(startVideo);
+        break;
+      case "stopEmpathize":
+        stopVideo();
+        break;
+    }
+  });
+
+  async function startVideo() {
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
+      video.srcObject = stream;
+      var counter = 0;
+      
+      setInterval(async () => {
+        try {
+          const detections = await faceapi
+            .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceExpressions();
+          var currentEmotions = detections[0].expressions;
+          Object.keys(runningAverages).forEach((key) => {
+            runningAverages[key] = calculateRunningAverage(
+              currentEmotions[key],
+              runningAverages[key],
+              counter
+            );
+          });
+
+          counter = counter + 1;
+
+          if (counter >= 25) {
+            chrome.runtime.sendMessage({type:"currentEmotion", emotion: runningAverages})
+            counter = 0;
+
+            Object.keys(runningAverages).forEach((key) => {
+              runningAverages[key] = currentEmotions[key];
+            });
+          }
+        } catch (err) {
+
+        }
+      }, 100);
+    } catch (err) {
+
+      if (err.name === "NotAllowedError") {
+        // User explicitly denied permission or dismissed the prompt
+        alert(
+          "Permission to access the camera was denied. Please check your settings."
+        );
+      } else {
+        // Other errors
+        alert("Failed to access the camera. Please check your settings.");
+      }
+    }
+  }
+
+  function calculateRunningAverage(currentValue, previousAverage, count) {
+    return (previousAverage * count + currentValue) / (count + 1);
+  }
+
+  function stopVideo() {
+    // You might want to stop the video stream when the switch is turned off
+    const stream = video.srcObject;
+    if (stream) {
+      const tracks = stream.getTracks();
+      tracks.forEach((track) => track.stop());
+    }
+    video.srcObject = null;
+  }
+});
---
+++ src/js/worker.js
@@ -0,0 +1,94 @@
+// Function to create an offscreen document
+var emotions = {};
+var currentEmotion = 'neutral';
+
+async function createOffscreen() {
+  // Check if offscreen document already exists
+  if (await chrome.offscreen.hasDocument()) return;
+
+  // Create offscreen document with specified URL, reasons, and justification
+  await chrome.offscreen.createDocument({
+    url: "/src/html/offscreen.html",
+    reasons: ["USER_MEDIA"], // Specify the reasons for creating the offscreen document
+    justification: "testing", // Justification for creating the offscreen document
+  });
+}
+
+// Listener for runtime messages
+chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
+  switch (msg.type) {
+    case "start":
+      sendResponse({ message: "Start message received." });
+
+      // Create offscreen document
+      await createOffscreen();
+
+
+      // Send message to start empathizing with offscreen document
+      await chrome.runtime.sendMessage({
+        type: "empathize",
+        offscreen: true,
+      });
+
+      return true;
+      break;
+    case "stop":
+      sendResponse({ message: "Stop message received." });
+
+      // Create offscreen document
+      await createOffscreen();
+
+      // Send message to stop empathizing with offscreen document
+      await chrome.runtime.sendMessage({
+        type: "stopEmpathize",
+        offscreen: true,
+      });
+
+
+      return true;
+      break;
+
+    case "currentEmotion":
+
+      emotions=msg.emotion;
+
+      let maxKey = null;
+      let secondMaxKey = null;
+      let maxValue = 0;
+
+    
+      for (const key in emotions) {
+          if (emotions[key] > maxValue) {
+              secondMaxKey = maxKey;
+              maxKey = key;
+              maxValue = emotions[key];
+              
+          }
+      }
+
+            currentEmotion = maxKey
+
+      sendResponse({ message: "getEmotion message received." });
+      
+
+      break;
+    case "contentScriptAskingForEmotion":
+
+      sendResponse({ message: currentEmotion });
+      break;
+    case "getResponse":
+      sendResponse({
+        message: "This is the response from the background script.",
+      });
+      break;
+  }
+});
+
+chrome.runtime.onSuspend.addListener(()=>{
+
+  chrome.storage.session.set({"buttonState":"stop"});
+  chrome.runtime.sendMessage({type: "refresh"} , function(response){
+
+  });
+});
+
