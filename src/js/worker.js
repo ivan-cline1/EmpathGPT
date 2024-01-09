92ff4b499a71e995179fc176ca89838581816a63
Ivan Cline <117295656+ivan-cline1@users.noreply.github.com>
2024-01-09 14:10:39 -0700

    comments

--- src/js/worker.js
+++ src/js/worker.js
@@ -1,7 +1,8 @@
-// Function to create an offscreen document
+//Initialize emotional information being held by the background script
 var emotions = {};
 var currentEmotion = "neutral";
 
+// Function to create an offscreen document
 async function createOffscreen() {
   // Check if offscreen document already exists
   if (await chrome.offscreen.hasDocument()) return;
@@ -14,7 +15,7 @@ async function createOffscreen() {
   });
 }
 
-// Listener for runtime messages
+// Listener for message from the popup
 chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
   switch (msg.type) {
     case "start":
@@ -23,7 +24,7 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
       // Create offscreen document
       await createOffscreen();
 
-      // Send message to start empathizing with offscreen document
+      // Send message to the offscreen document to start empathizing based on the users webcam information
       await chrome.runtime.sendMessage({
         type: "empathize",
         offscreen: true,
@@ -34,10 +35,10 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
     case "stop":
       sendResponse({ message: "Stop message received." });
 
-      // Create offscreen document
+      // IF the user presses "Off" before "On" Create offscreen document, and just dont allow webcam info to be read
       await createOffscreen();
 
-      // Send message to stop empathizing with offscreen document
+      // Send message to stop empathizing with offscreen document/turn off the camera
       await chrome.runtime.sendMessage({
         type: "stopEmpathize",
         offscreen: true,
@@ -45,10 +46,10 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
 
       return true;
       break;
-
+  //message is sent from the "offscreen" script, as it will send the average currentEmotion to the background on an interval
     case "currentEmotion":
       emotions = msg.emotion;
-
+      // hoping to add a better way of picking an emotion for the content script to recieve
       let maxKey = null;
       let secondMaxKey = null;
       let maxValue = 0;
@@ -66,6 +67,7 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
       sendResponse({ message: "getEmotion message received." });
 
       break;
+      //When the content script is asking for an emotion (this happens when a user is typing into the chatgpt input box), send the currentEmotion
     case "contentScriptAskingForEmotion":
       sendResponse({ message: currentEmotion });
       break;
@@ -76,7 +78,7 @@ chrome.runtime.onMessage.addListener(async (msg, sender, sendResponse) => {
       break;
   }
 });
-
+//When the app ends, set the button state to stop and refresh the content.js webpage so we dont have a rogue content script.
 chrome.runtime.onSuspend.addListener(() => {
   chrome.storage.session.set({ buttonState: "stop" });
   chrome.runtime.sendMessage({ type: "refresh" }, function (response) {});
